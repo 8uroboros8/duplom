876306c97a2fa6e6ffea20b70230be9ed37e250b
Parhomenko <parhomenko@cv.oe.ua>
2024-06-12 10:08:52 +0300

    add models to admin

--- fft_bot/gamers/admin.py
+++ fft_bot/gamers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gamers.models import Gamers, Games, TeleData
 
 # Register your models here.
+admin.site.register(Gamers)
+admin.site.register(Games)
+admin.site.register(TeleData)
